b3cf1b7c61e36b0b90da630bcde6251bf083cf94
nikitha-nikki12 <nikithathalari257@gmail.com>
2024-10-22 11:30:57 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,18 @@
+Write a program to get a string value and print the same as output:
+Sample Input:
+Hello World
+Sample Output:
+Hello World
+a="Hello World"
+print(a)
+
+
+
+2) Write a program to print th length of the given string.
+Sample Input:
+Hello
+Sample Output:
+Hello: 5
+a="Hello"
+b=len(a)
+print(f"{a}:{b}")
